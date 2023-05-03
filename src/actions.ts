06be7f3f9046189c4c4b63d35a6afba25e735bb6
Dan Prince <1266011+danprince@users.noreply.github.com>
2023-05-03 11:36:38 +0300

    adds bump action

--- src/actions.ts
+++ src/actions.ts
@@ -1,4 +1,4 @@
-import { tween } from "@danprince/games";
+import { easeInOut, tween } from "@danprince/games";
 import { GameObject } from "./game";
 
 export function moveBy(object: GameObject, dx: number, dy: number): boolean {
@@ -7,11 +7,15 @@ export function moveBy(object: GameObject, dx: number, dy: number): boolean {
 
 export function moveTo(object: GameObject, x: number, y: number): boolean {
   if (game.map.isOutOfBounds(x, y)) return false;
-  let dx = (object.x - x);
-  let dy = (object.y - y);
+  let cell = game.map.getCell(x, y);
+  if (cell == null) return false;
 
-  object.spriteOffsetX = dx;
-  object.spriteOffsetY = dy;
+  if (cell.objects.length) {
+    return bump(object, x, y);
+  }
+
+  object.spriteOffsetX = object.x - x;
+  object.spriteOffsetY = object.y - y;
 
   tween(object, {
     spriteOffsetX: 0,
@@ -19,9 +23,26 @@ export function moveTo(object: GameObject, x: number, y: number): boolean {
   }, {
     duration: 150,
     id: object.id,
-    step: t => object.spriteOffsetZ = Math.sin(t * Math.PI) * 0.2,
+    step: t => object.spriteOffsetZ = Math.sin(t * Math.PI) * 0.25,
+    easing: easeInOut,
   });
 
   game.map.moveObject(object, x, y);
   return true;
+}
+
+export function bump(object: GameObject, x: number, y: number): boolean {
+  object.spriteOffsetX = (x - object.x) / 2;
+  object.spriteOffsetY = (y - object.y) / 2;
+
+  tween(object, {
+    spriteOffsetX: 0,
+    spriteOffsetY: 0,
+  }, {
+    duration: 150,
+    id: object.id,
+    easing: easeInOut,
+  });
+
+  return false;
 }

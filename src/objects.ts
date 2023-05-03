f18cd13029cf28b4d12ac9706b87ea75d4bf0486
Dan Prince <1266011+danprince@users.noreply.github.com>
2023-05-03 14:55:26 +0300

    fix sprite name conventions

--- src/objects.ts
+++ src/objects.ts
@@ -4,7 +4,7 @@ import * as sprites from "./sprites";
 
 export function Player(): GameObject {
   let unit = new GameObject();
-  unit.sprite = sprites.mob_druid_idle_1;
+  unit.sprite = sprites.object_druid_idle_1;
   unit.tags.add(Tags.Mobile);
   return unit;
 }
